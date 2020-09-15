8764d6210c7fc3d9d480fa6e7f03f33264e6baa0
see-bet-code <brittani.elan.taylor@gmail.com>
2020-09-15 13:26:23 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_accessor :roster, :name
+
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+    
+    def add_student(student, grade)
+        @roster[grade] = [] if !@roster[grade]
+        @roster[grade] << student
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            @roster[grade] = students.sort
+        end
+        @roster.sort.to_h
+    end
+end
